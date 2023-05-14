023b48945ac4d9efdcdb60fae3ab3e0dae2c233e
pratiushp <pratiushprasain@gmail.com>
2023-05-14 18:46:54 +0545

    2nd Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,171 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [result, setResult] = useState("");
+
+  const handleClick = (e) => setResult(result.concat(e.target.id));
+
+  // Clear all result //
+  const clear = () => setResult("");
+
+  // Delete Element by 1 Function //
+  const deleteElement = () => setResult(result.slice(0, -1));
+
+  //calculate the result
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (error) {
+      setResult("error");
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="flex flex-col justify-centerflex justify-center items-center h-screen">
+      <h1 className="font-bold text-2xl mb-4">Calculator</h1>
+      <div className=" w-96 mx-auto bg-black rounded-lg  ">
+        <div className="p-3 border-solid">
+          <input
+            type="text"
+            value={result}
+            disabled
+            className=" w-full mb-4 p-2 text-right border-solid bg-slate-200  pt-2 rounded-md"
+          />
+        </div>
+        <div className="button grid grid-cols-3 gap-2 p-2">
+          <button
+            id="1"
+            className="font-bold p-2 rounded-md text-white bg-blue-500 hover:bg-blue-600"
+            onClick={handleClick}
+          >
+            1
+          </button>
+          <button
+            id="2"
+            className=" font-bold p-2 rounded-md text-white bg-blue-500 hover:bg-blue-600"
+            onClick={handleClick}
+          >
+            2
+          </button>
+          <button
+            id="3"
+            className=" font-bold p-2 rounded-md text-white bg-blue-500 hover:bg-blue-600"
+            onClick={handleClick}
+          >
+            3
+          </button>
+          <button
+            id="4"
+            className=" font-bold p-2 rounded-md text-white bg-blue-500 hover:bg-blue-600"
+            onClick={handleClick}
+          >
+            4
+          </button>
+          <button
+            id="5"
+            className="font-bold p-2 rounded-md text-white bg-blue-500 hover:bg-blue-600"
+            onClick={handleClick}
+          >
+            5
+          </button>
+          <button
+            id="6"
+            className="font-bold p-2 rounded-md text-white bg-blue-500 hover:bg-blue-600"
+            onClick={handleClick}
+          >
+            6
+          </button>
+          <button
+            id="7"
+            className="font-bold p-2 rounded-md text-white bg-blue-500 hover:bg-blue-600"
+            onClick={handleClick}
+          >
+            7
+          </button>
+          <button
+            id="8"
+            className="font-bold p-2 rounded-md text-white bg-blue-500 hover:bg-blue-600"
+            onClick={handleClick}
+          >
+            8
+          </button>
+          <button
+            id="9"
+            className="font-bold p-2 rounded-md text-white bg-blue-500 hover:bg-blue-600"
+            onClick={handleClick}
+          >
+            9
+          </button>
 
-export default App
+          <button
+            id="."
+            className="font-bold p-2 rounded-md text-white bg-blue-500 hover:bg-blue-600"
+            onClick={handleClick}
+          >
+            .
+          </button>
+          <button
+            id="0"
+            className="font-bold p-2 rounded-md text-white bg-blue-500 hover:bg-blue-600"
+            onClick={handleClick}
+          >
+            0
+          </button>
+
+          <button
+            id="+"
+            className="font-bold p-2 rounded-md text-white bg-yellow-500 hover:bg-yellow-600"
+            onClick={handleClick}
+          >
+            +
+          </button>
+
+          <button
+            id="*"
+            className="font-bold p-2 rounded-md text-white bg-yellow-500 hover:bg-yellow-600"
+            onClick={handleClick}
+          >
+            *
+          </button>
+
+          <button
+            id="-"
+            className="font-bold p-2 rounded-md text-white bg-yellow-500 hover:bg-yellow-600"
+            onClick={handleClick}
+          >
+            -
+          </button>
+
+          <button
+            id="/"
+            className="font-bold p-2 rounded-md text-white bg-yellow-500 hover:bg-yellow-600"
+            onClick={handleClick}
+          >
+            /
+          </button>
+
+          <button
+            className="font-bold p-2 rounded-md text-white bg-gray-500 hover:bg-gray-600"
+            onClick={clear}
+          >
+            AC
+          </button>
+          <button
+            className="font-bold p-2 rounded-md text-white bg-gray-500 hover:bg-gray-600"
+            onClick={deleteElement}
+          >
+            DEL
+          </button>
+
+          <button
+            id="="
+            className="font-bold p-2 rounded-md text-white bg-gray-500 hover:bg-gray-600"
+            onClick={calculate}
+          >
+            =
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
